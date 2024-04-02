4778a9358b638c6a545050bacf32592a44a0b8ef
jpb <jakub.baranski.33@gmail.com>
2024-04-02 19:04:40 +0100

    Fixes to titlebar

--- UTE/MainWindow.xaml.cs
+++ UTE/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace UltraTextEdit
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : RibbonWindow
+    public partial class MainWindow : Window
     {
         bool isDebug;
 
